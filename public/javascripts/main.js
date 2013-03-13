c241895eb0d964076f37ee31f694aaa0549552e0
William Youmans <will@williamyoumans.com>
2013-03-13 02:32:19 -0600

    cool image gallery animations

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,8 +2,20 @@
 
   $(function() {
     $('html').removeClass('no-js').addClass('yes-js');
+
     $('.container').hide().delay(100).fadeIn(800);
-    $('.photo-album a').nyroModal();
+
+    $('.photo-album .photo').nyroModal();
+
+    $('.photo-album .photo').mouseenter(function(e) {
+      $(this).find('.zoom').stop(true, false).animate({
+        bottom: '50%'
+      }, 'fast')
+    }).mouseleave(function(e) {
+      $(this).find('.zoom').stop(true, false).animate({
+        bottom: '-29px'
+      }, 'fast')
+    });
   });
 
 })();
